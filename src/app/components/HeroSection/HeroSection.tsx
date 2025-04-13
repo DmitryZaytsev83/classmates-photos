ac652e922bd92b90435cf5345caae149afa4fef3
Dmitry Zaytsev <dmitry.zaytsev83@gmail.com>
2025-04-13 19:16:34 +0300

    add HeroSection component

---
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,22 @@
+// app/components/HeroSection.tsx
+import {Typography} from 'antd';
+import GridContainer from "@/app/components/GridContainer/GridContainer";
+
+const {Title, Paragraph} = Typography;
+
+export default function HeroSection() {
+    return (
+        <div
+            style={{
+                textAlign: 'center',
+                padding: '100px 20px'
+            }}>
+            <GridContainer>
+                <Title level={1}>Добро пожаловать!</Title>
+                <Paragraph>
+                    Это сайт для учеников школы №70 города Рязани выпуска 2000 года.
+                </Paragraph>
+            </GridContainer>
+        </div>
+    );
+}

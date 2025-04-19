b106f6905de7d54bd13c71d6476985609726f697
Dmitry Zaytsev <dmitry.zaytsev83@gmail.com>
2025-04-19 22:52:04 +0300

    add dummy photo page and info page

---
+++ src/app/info/page.tsx
@@ -0,0 +1,15 @@
+import GridContainer from '@/components/GridContainer/GridContainer';
+
+import styles from './Info.module.css';
+
+const InfoPage = () => {
+    return (
+        <div className={styles.wrapper}>
+            <GridContainer>
+                InfoPage
+            </GridContainer>
+        </div>
+    );
+};
+
+export default InfoPage;
---
+++ src/app/photo/page.tsx
@@ -0,0 +1,15 @@
+import GridContainer from '@/components/GridContainer/GridContainer';
+
+import styles from './Photo.module.css';
+
+const PhotoPage = () => {
+    return (
+        <div className={styles.wrapper}>
+            <GridContainer>
+                PhotoPage
+            </GridContainer>
+        </div>
+    );
+};
+
+export default PhotoPage;
